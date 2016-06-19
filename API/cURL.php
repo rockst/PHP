19dac729677dee574dcef530e3c17ffa7242d701
FLT-Rock <rock.lin@fullerton.com.tw>
2016-06-19 12:25:52 +0800

    new curl post method

--- API/cURL.php
+++ API/cURL.php
@@ -6,3 +6,11 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $result = curl_exec($ch);
 print_r(json_decode($result));
+
+$ch = curl_init('http://localhost:8888/cosmed/get_cosmed_number.php');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true); // 啟用POST
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query( array( "oid"=>"123") ));
+$result = curl_exec($ch);
+curl_close($ch);
+print_r(json_decode($result,1));
